cd949bc0aa8774ffeacca9ec6050a10fa60a2b99
Ernesto Celis <developer@celisdelafuente.net>
2015-03-19 15:53:45 -0600

    WIP unit tests

---
+++ CirrusLoad/src/test/java/org/compiere/util/confHL7/MessageSenderTest.java
@@ -0,0 +1,34 @@
+package org.compiere.util.confHL7;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MessageSenderTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
