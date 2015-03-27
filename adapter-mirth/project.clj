0eee39c79ceaf311a522b2f8e4648b0bc1af2bdb
Ernesto Angel Celis de la Fuente <developer@celisdelafuente.net>
2015-03-27 13:45:30 -0600

    FIX more mirth dependencies

--- adapter-mirth/project.clj
+++ adapter-mirth/project.clj
@@ -4,7 +4,13 @@
   :license {:name "GNU Affero Public General License version"
             :url "http://www.gnu.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [log4j/log4j "1.2.17"]
                  [com.mirth.commons.encryption/mirth-crypto "1"]
+                 [com.thoughtworks.xstream/xstream "1.4.8"]
+                 [org.apache.commons/commons-lang3 "3.3.2"]
+                 [org.apache.httpcomponents/httpcore "4.4.1"]
+                 [org.apache.httpcomponents/httpmime "4.4"]
+                 [org.apache.httpcomponents/httpclient "4.4"]
                  [com.mirth.connect/mirth-donkey "3.2.1svn"]
                  [com.mirth.connect/mirth-server "3.2.1svn"]
                  [com.mirth.connect/mirth-client "3.2.1svn"]]
