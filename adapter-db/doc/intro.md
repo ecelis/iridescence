2d3e434593b949573cf3d38166a745efda828b8d
Ernesto Angel Celis de la Fuente <developer@celisdelafuente.net>
2015-04-15 18:11:42 -0500

    WIP Adapter or Databases

---
+++ adapter-db/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to adapter-db
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ adapter-db/src/adapter_db/core.clj
@@ -0,0 +1,11 @@
+(ns adapter-db.core
+  (:require [clojure.java/jdbc :as jdbc]
+            [honeysql.core :as sql]
+            [honeysql.helpers :refer :all])
+  (:gen-class))
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
