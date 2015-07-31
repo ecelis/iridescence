da0099740eae01d5b5f4ebb3eea933d83fa8d562
Ernesto Celis <developer@celisdelafuente.net>
2015-07-31 04:50:33 -0500

    UPDATE build.sh to add clean task

--- build.sh
+++ build.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-for dep in adapter-csv adapter-db adapter-hl7v2;
-do
-  cd $dep
-  pwd
-  lein clean; lein deps; lein uberjar; lein install
-  cd ..
-done
+PROJECTS="adapter-csv adapter-db adapter-hl7v2"
+case $1 in
+"clean")
+	for dep in $PROJECTS;
+	do
+	  cd $dep
+	  echo "$(pwd) clean..."
+	  lein clean;
+	  cd ..
+	done
+	cd webui
+	lein clean;
+	;;
+*)
+	for dep in $PROJECTS;
+	do
+	  cd $dep
+	  pwd
+	  lein clean; lein deps; lein uberjar; lein install
+	  cd ..
+	done
+	cd webui
+  echo "$(pwd) clean..."
+	lein clean; lein deps; lein ring uberjar
+	;;
+esac
 
-cd webui
-lein clean; lein deps; lein ring uberjar
