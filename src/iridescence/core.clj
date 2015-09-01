3301512c400494c2db0b82502d77028e1268e3bb
Ernesto Celis <ecelis@ecaresoft.com.mx>
2015-09-01 10:40:53 -0500

    ADD Core tests

--- src/iridescence/core.clj
+++ src/iridescence/core.clj
@@ -1,4 +1,5 @@
-(ns iridescence.core)
+(ns iridescence.core
+  (:require [clojure.java.io :as io]))
 
 (def savedir "/tmp/smartconnector/") ; TODO Set a definitive path
 (def wsdir (str savedir "/workspace/"))
@@ -6,4 +7,25 @@
 (def outdir (str savedir "/out/"))
 (def tpldir (str savedir "/templates/"))
 
+(defn ls "List files in a given path" [path]
+  (def fs (file-seq (io/file path)))
+  [fs])
+
+(defn storage-setup "Set up storage paths" []
+  ; TODO this if nil are awful
+  (if (.isDirectory (io/file wsdir))
+    nil
+    (.mkdir (java.io.File. savedir)))
+  (if (.isDirectory (io/file wsdir))
+    nil
+    (.mkdir (java.io.File. wsdir)))
+  (if (.isDirectory (io/file wipdir))
+    nil
+    (.mkdir (java.io.File. wipdir)))
+  (if (.isDirectory (io/file outdir))
+    nil
+    (.mkdir (java.io.File. outdir)))
+  (if (.isDirectory (io/file tpldir))
+    nil
+    (.mkdir (java.io.File. tpldir))))
 
